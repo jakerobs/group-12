4852dab7eaaddbf979b72a27a2967a9d69917a79
zanderson1998 <zanderson1998@yahoo.com>
2020-12-17 19:23:48 -0700

    add category page

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+//fetch info from http://jservice.io/api/"category"
+//take fetched information and update the quizpage
+
+
+//autocorrect fetch
+//update input box with fetch value
+
+
+//save high scores
+
+//load high scores
+
+//add functioning to landing page

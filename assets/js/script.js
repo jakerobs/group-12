a1f31442558b96d303c1820056644a30e6237c74
zanderson1998 <zanderson1998@yahoo.com>
2020-12-17 21:11:42 -0700

    start javascript fetch calls to load categories

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,56 @@
+var gradeButtonEl = document.querySelectorAll("#btn-grade");
+
+
+//fetch call to get random categories and assign them to category headers
+var categories = function(){
+    fetch("http://jservice.io/api/categories?count=4").then(function(response) { 
+        if (response.ok){
+            response.json().then(function(data){
+                //replace text data on categories page with new categories
+                console.log(data);
+                document.querySelector("#category-one").textContent = data[0].title;
+                document.querySelector("#category-two").textContent = data[1].title;
+                document.querySelector("#category-three").textContent = data[2].title;
+                document.querySelector("#category-four").textContent = data[3].title;
+                var categoryOneId = data[0].id;
+                var categoryTwoId = data[0].id;
+                var categoryThreeId = data[0].id;
+                var categoryFourId = data[0].id;
+                return categoryOneId, categoryTwoId, categoryThreeId, categoryFourId;
+            });
+        } else{
+            //change this alert to be modal later
+            alert("Error: " + response.statusText);
+        }
+    });
+};
+
+var categoryIds = function(categoryOneId, categoryTwoId, categoryThreeId, categoryFourId) {
+    //do something with the ids
+};
+
+
 //fetch info from http://jservice.io/api/"category"
 //take fetched information and update the quizpage
+var gradeButtonClickHandler = function(event){
+    console.log(event.target.parentNode.id);
+    if (event.target.parentNode.id === "category-1"){
+        //do this
+        console.log(categoryOneId);
+    } 
+    else if (event.target.parentNode.id === "category-2"){
+        //do this
+        console.log(categoryTwoId);
+    }
+    else if (event.target.parentNode.id === "category-3"){
+        //do this
+        console.log(categoryThreeId);
+    }
+    else if (event.target.parentNode.id === "category-4"){
+        //do this
+        console.log(categoryFourId);
+    }
+};
 
 
 
@@ -12,3 +63,11 @@
 //load high scores
 
 //add functioning to landing page
+
+
+gradeButtonEl.forEach(function(el){
+    el.addEventListener("click",gradeButtonClickHandler);
+});
+
+//call categories
+categories();

35a428b6ee1228a89e5072590ed1f116367dc743
zanderson1998 <zanderson1998@yahoo.com>
2020-12-23 12:34:36 -0600

    add incorrect/correct answer check and change some styling

--- assets/js/landingpagescript.js
+++ assets/js/landingpagescript.js
@@ -1,7 +1,15 @@
 var startButtonEl = document.querySelector("#start-btn");
+var leaderBoardButtonEl = document.querySelector("#leaderboard-btn");
 
 var startNewGame = function () {
+    //remove the old categories to refresh the page with new ones. 
     localStorage.removeItem("categoryIds","categoryTitles");
+    window.location.replace("./category.html");
 }
-
+//start a new game when the start button is clicked
 startButtonEl.addEventListener("click",startNewGame);
+
+//change html page when leaderboard button is clicked
+leaderBoardButtonEl.addEventListener("click", function() {
+    window.location.replace("./leaderboard.html");
+});
--- assets/js/quizpagescript.js
+++ assets/js/quizpagescript.js
@@ -1,15 +1,54 @@
-var userAnswer = document.querySelector("#answer").textContent;
+var userAnswerEl = document.querySelector("#user-form");
+var answerInputBox = document.getElementById("answer");
+var answerCheck = document.querySelector("#answer-check");
 
-var getQuestionData = function(){
-    //get question data from url
-    var queryString = window.location.search;
-    var urlParams = new URLSearchParams(queryString);
-    var categoryTitle = urlParams.get("categoryTitle");
-    var categoryQuestion = urlParams.get("categoryQuestion");
-    var categoryAnswer = urlParams.get("categoryAnswer");
+//Get answers from url 
+var queryString = window.location.search;
+var urlParams = new URLSearchParams(queryString);
+var categoryTitle = urlParams.get("categoryTitle");
+var categoryQuestion = urlParams.get("categoryQuestion");
+var categoryAnswer = urlParams.get("categoryAnswer");
+document.querySelector("#trivia-question").textContent = "Question: " + categoryQuestion;
+document.querySelector("#trivia-categories").textContent = "The category is: " + categoryTitle;
 
-    document.querySelector("#trivia-question").textContent = "Question: " + categoryQuestion;
-    document.querySelector("#trivia-categories").textContent = "The category is: " + categoryTitle;
-    return categoryTitle, categoryAnswer;
+
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+
+    //get value from form input
+    var userAnswerEl = answerInputBox.value.trim();
+
+    if (userAnswerEl) {
+        checkAnswer(userAnswerEl);
+        answerInputBox.value = "";
+    } else {
+        alert("Please Enter an Answer!");
+    }
 };
-getQuestionData();
+
+var checkAnswer = function (userAnswerEl) {
+    categoryAnswerLower = categoryAnswer.toLowerCase();
+    userAnswerLower = userAnswerEl.toLowerCase();
+    // var categoryAnswerLower = categoryAnswer.toLowerCase;
+    // var userAnswerLower = userAnswerEl.toLowerCase;
+
+    if (userAnswerLower === categoryAnswerLower) {
+        console.log("Good work");
+        answerCheck.innerHTML = "";
+        var answerCheckEl = document.createElement("h4");
+        answerCheckEl.textContent = "Correct!";
+        answerCheckEl.classList.add("column","has-text-success");
+        answerCheck.appendChild(answerCheckEl);
+    }
+    else {
+        console.log("Not Good Work");
+        answerCheck.innerHTML = "";
+        var answerCheckEl = document.createElement("h4");
+        answerCheckEl.textContent = "Incorrect!";
+        answerCheckEl.classList.add("column","has-text-danger");
+        answerCheck.appendChild(answerCheckEl);
+    }
+};
+
+//getQuestionData();
+userAnswerEl.addEventListener("submit", formSubmitHandler);

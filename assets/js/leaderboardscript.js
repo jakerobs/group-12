b8446810bc385d906f1f7d7bfcc144354ebad710
zanderson1998 <zanderson1998@yahoo.com>
2021-01-07 15:25:32 -0700

    add name request function if your name is not already inputted into the game

--- assets/js/leaderboardscript.js
+++ assets/js/leaderboardscript.js
@@ -6,6 +6,10 @@ leaderboardArray.sort(function(a, b){return b - a});
 
 //add the items to the dom
 var leaderboardPopulator = function () {
+    var name = JSON.parse(localStorage.getItem("name"));
+    if (!name){
+        getName();
+    }
     for (var i = 0; i < leaderboardArray.length; i++) {
         var containerEl = document.createElement("div");
         containerEl.classList.add("grid-container");
@@ -18,15 +22,13 @@ var leaderboardPopulator = function () {
         var nameEl = document.createElement("div");
         nameEl.classList.add("grid-child");
         nameEl.classList.add("name");
-        nameEl.textContent = JSON.parse(localStorage.getItem("name"));
+        nameEl.textContent = name;
         //add avatar next
         var avatarEl = document.createElement("img");
         avatarEl.classList.add("grid-child");
         avatarEl.classList.add("avatar");
         avatarEl.classList.add("center");
-        avatarEl.src = `https://robohash.org/${"name"}`;
-        
-    // add link to image here //////////////////////////////////////////////////
+        avatarEl.src = "https://robohash.org/${" + name + "}";
         //append name and then score to container
         containerEl.appendChild(nameEl);
         containerEl.appendChild(scoreEl);
@@ -34,8 +36,41 @@ var leaderboardPopulator = function () {
         //append to page
         document.querySelector("#leaderboard-body").appendChild(containerEl);
     }
-    
-
 }
+
+var getName = function(){
+    //create modal to get name value
+    name = "NaN";
+    var leaderboardNameModalEl = document.createElement("div");
+    leaderboardNameModalEl.classList.add("modal");
+    //create input box and button to submit
+    var buttonHolderEl = document.createElement("div");
+    buttonHolderEl.classList.add("columns");
+    //add text
+    var nameDisplayEl = document.createElement("h3");
+    nameDisplayEl.textContent = "Enter Your Name Below";
+    //add buttons to button holder
+    var nameInputBox = document.createElement("input");
+    nameInputBox.classList.add("column");
+    nameInputBox.classList.add("is-three-fifths");
+    nameInputBox.classList.add("button");
+    nameInputBox.classList.add("clear-btn");
+    nameInputBox.type = "text";
+    nameInputBox.placeholder = "Enter Your Name";
+    var nameSubmitButton = document.createElement("a");
+    nameSubmitButton.classList.add("button");
+    nameSubmitButton.classList.add("column");
+    nameSubmitButton.classList.add("is-two-fifth");
+    nameSubmitButton.classList.add("avatar-btn");
+    nameSubmitButton.onclick = function(){localStorage.setItem("name",JSON.stringify(nameInputBox.value)); window.location.reload();};
+    nameSubmitButton.textContent = "Submit";
+    //append objects to button holder
+    buttonHolderEl.appendChild(nameInputBox);
+    buttonHolderEl.appendChild(nameSubmitButton);
+    //append to dom
+    leaderboardNameModalEl.appendChild(nameDisplayEl);
+    leaderboardNameModalEl.appendChild(buttonHolderEl);
+    document.querySelector("#leaderboard-page").appendChild(leaderboardNameModalEl);
+};
 
 leaderboardPopulator();
